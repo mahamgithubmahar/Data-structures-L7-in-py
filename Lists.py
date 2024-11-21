d61c365d65b97dbe379d7606fa8465905ac16f3c
mahamgithubmahar <mahamgithub@gmail.com>
2024-11-21 17:58:10 +0400

    y

---
+++ Lists.py
@@ -0,0 +1,13 @@
+list1=["Mango","Papaya","Banana"]
+print(len(list1))
+print(list1[0])
+print(list1[-1])
+list1.sort()
+print(list1)
+print(list1*2)
+list1.remove("Papaya")
+print(list1)
+list1.append("Guava")
+print(list1)
+print(list1[:2])
+print(list1.clear())
---
+++ dictionary.py
@@ -0,0 +1,8 @@
+my_dic1={"Name":"Maham","Grade":9}
+print(my_dic1)
+print(my_dic1["Name"])
+print(my_dic1["Grade"])
+my_dic1["Grade"]=10
+print(my_dic1)
+my_dic1.clear()
+print(my_dic1)
